fe8a1983df7bd6db2da9e41c3a81b65d1308b8b0
Lucas Corrêa <78830365+lucascorreanasario@users.noreply.github.com>
2022-09-08 19:12:31 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function menuPrincipal() {
+    if (document.getElementById('menuPrincipal').style.display=="") {
+        document.getElementById('menuPrincipal').style.display="none";
+    } else {
+        document.getElementById('menuPrincipal').style.display="";
+    }
+}
